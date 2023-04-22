b545c197d19c3c838a8f041ce78b11b6911514d5
K.D. Hubbard <hubbard.kd@gmail.com>
2023-04-22 17:49:44 -0600

    remove pseudocode and single quotes

--- lib/games_stats.rb
+++ lib/games_stats.rb
@@ -1,4 +1,3 @@
-
 require_relative "futbol"
 
 class GamesStats < Futbol
@@ -7,19 +6,11 @@ class GamesStats < Futbol
     super(locations)
   end
 
-  # highest sum of winning and losing teams score
   def highest_total_score
-    # i want to find the game with the highest points scored and add the home score and away score.
     games.map do |game|
       game.away_team_goals + game.home_team_goals
     end.sort.last
   end
-
-  # count of games by season
-  # =
-  # has with season names as keys
-  # counts of games as values
-  # ex. 20122013 => 32
 
   def count_of_games_by_season
     game_count = Hash.new
